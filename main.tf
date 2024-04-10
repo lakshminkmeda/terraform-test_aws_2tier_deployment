df193ec5c86a3b776c2a8aa91a8f2afe491b735e
Lakshmi N K Meda <162011001+lakshminkmeda@users.noreply.github.com>
2024-04-10 11:10:57 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,43 +1,43 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = ">4.16"
     }
   }
 }
 
 provider "aws" {
-  shared_credentials_files = "~/.aws/credentials"
-  region = "eu-north-1"
+  shared_credentials_files = ["~/.aws/credentials"]
+  region                   = "eu-north-1"
 }
 
 # Create vpc, 2 public subnets in 2 different AZ's, 1 private subnet in each of those AZ
 resource "aws_vpc" "a_vpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
-    Name = a_vpc
+    Name = "a_vpc"
   }
 }
 resource "aws_subnet" "pubsub01" {
-  vpc_id = aws_vpc.a_vpc.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id            = aws_vpc.a_vpc.id
+  cidr_block        = "10.0.1.0/24"
   availability_zone = "eu-north-1a"
   tags = {
     Name = "public_subnet-01"
   }
 }
 resource "aws_subnet" "pubsub02" {
-  vpc_id = aws_vpc.a_vpc.id
-  cidr_block = "10.0.2.0/24"
+  vpc_id            = aws_vpc.a_vpc.id
+  cidr_block        = "10.0.2.0/24"
   availability_zone = "eu-north-1b"
   tags = {
     Name = "public_subnet_02"
   }
 }
 resource "aws_subnet" "prvsub01" {
-  vpc_id = aws_vpc.a_vpc.id
-  cidr_block = "10.0.3.0/24"
+  vpc_id            = aws_vpc.a_vpc.id
+  cidr_block        = "10.0.3.0/24"
   availability_zone = "eu-north-1a"
   tags = {
     Name = "private_subnet_01"
@@ -45,8 +45,8 @@ resource "aws_subnet" "prvsub01" {
   map_public_ip_on_launch = false
 }
 resource "aws_subnet" "prvub02" {
-  vpc_id = aws_vpc.a_vpc.id
-  cidr_block = "10.0.4.0/24"
+  vpc_id            = aws_vpc.a_vpc.id
+  cidr_block        = "10.0.4.0/24"
   availability_zone = "eu-north-1b"
   tags = {
     Name = "private_subnet_02"
@@ -65,48 +65,48 @@ resource "aws_route_table" "rt_public_ig" {
   vpc_id = aws_vpc.a_vpc.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.ig_pc.id
+    gateway_id = aws_internet_gateway.ig_vpc.id
   }
   tags = {
     Name = "routetable_public_internetgateway"
   }
 }
 resource "aws_route_table_association" "rt_assoc1_pubsub01" {
-  subnet_id = aws_subnet.pubsub01.id
-  route_table_id = aws_route_table.rt_public_ig
+  subnet_id      = aws_subnet.pubsub01.id
+  route_table_id = aws_route_table.rt_public_ig.id
 }
 resource "aws_route_table_association" "rt_assoc1_pubsub02" {
-  subnet_id = aws_subnet.pubsub02.id
-  route_table_id = aws_route_table.rt_public_ig
+  subnet_id      = aws_subnet.pubsub02.id
+  route_table_id = aws_route_table.rt_public_ig.id
 }
 
 # Security group for vpc
 resource "aws_security_group" "publicsg_vpc" {
-  name = "publicsg_vpc"
+  name        = "publicsg_vpc"
   description = "Security group for allowing traffic from VPC"
-  vpc_id = aws_vpc.a_vpc.id
-  depends_on = [ aws_vpc.a_vpc ]
+  vpc_id      = aws_vpc.a_vpc.id
+  depends_on  = [aws_vpc.a_vpc]
   ingress {
     from_port = "0"
-    to_port = "0"
-    protocol = "-1"
+    to_port   = "0"
+    protocol  = "-1"
   }
   ingress {
-    from_port = "80"
-    to_port = "80"
-    protocol = "tcp"
+    from_port   = "80"
+    to_port     = "80"
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    from_port = "22"
-    to_port = "22"
-    protocol = "tcp"
+    from_port   = "22"
+    to_port     = "22"
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
-    from_port = "0"
-    to_port = "0"
-    protocol = "-1"
+    from_port   = "0"
+    to_port     = "0"
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
   tags = {
@@ -116,20 +116,20 @@ resource "aws_security_group" "publicsg_vpc" {
 
 # Security group for Application Load balancer
 resource "aws_security_group" "sg_alb" {
-  name = "sg_alb"
+  name        = "sg_alb"
   description = "Security group for Application Load Balancer"
-  vpc_id = aws_vpc.a_vpc.id
-  depends_on = [ aws_vpc.a_vpc ]
+  vpc_id      = aws_vpc.a_vpc.id
+  depends_on  = [aws_vpc.a_vpc]
   ingress {
-    from_port = "0"
-    to_port = "0"
-    protocol = "-1"
+    from_port   = "0"
+    to_port     = "0"
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
-    from_port = "0"
-    to_port = "0"
-    protocol = "-1"
+    from_port   = "0"
+    to_port     = "0"
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
   tags = {
@@ -139,10 +139,10 @@ resource "aws_security_group" "sg_alb" {
 
 # Create EC2 instance for public subnet 01
 resource "aws_instance" "web_server_01" {
-  ami = "ami-0914547665e6a707c"
-  instance_type = "t3.micro"
+  ami             = "ami-0914547665e6a707c"
+  instance_type   = "t3.micro"
   security_groups = [aws_security_group.publicsg_vpc.id]
-  subnet_id = aws_subnet.pubsub01.id
+  subnet_id       = aws_subnet.pubsub01.id
 
   user_data = <<-EOF
     #¡/bin/bash
@@ -155,10 +155,10 @@ resource "aws_instance" "web_server_01" {
 }
 # Create EC2 instance for public subnet 02
 resource "aws_instance" "web_server_02" {
-  ami = "ami-0914547665e6a707c"
-  instance_type = "t3.micro"
+  ami             = "ami-0914547665e6a707c"
+  instance_type   = "t3.micro"
   security_groups = [aws_security_group.publicsg_vpc.id]
-  subnet_id = aws_subnet.pubsub02.id
+  subnet_id       = aws_subnet.pubsub02.id
 
   user_data = <<-EOF
     #¡/bin/bash
@@ -170,3 +170,54 @@ resource "aws_instance" "web_server_02" {
     EOF
 }
 
+# Create Application Load Balancer & target groups
+resource "aws_lb" "alb_web" {
+  name = "alb_web"
+  internal = false
+  load_balancer_type = "application"
+  security_groups = [aws_security_group.sg_alb]
+  subnets = [aws_subnet.pubsub01.id,aws_subnet.pubsub02.id]
+  tags = {
+    Name = "alb"
+  }
+}
+resource "aws_lb_target_group" "vpc_target_grp" {
+  name = "vpc_target_grp"
+  vpc_id = aws_vpc.a_vpc.id
+  port = "80"
+  protocol = "HTTP"
+}
+resource "aws_lb_target_group" "alb_target_grp" {
+  name = "alb_target_grp"
+  vpc_id = aws_vpc.a_vpc.id
+  port = "80"
+  protocol = "HTTP"
+  depends_on = [ aws_vpc.a_vpc ]
+  health_check {
+    interval = 30
+    path = "/var/www/html/index.html"
+    timeout = 60
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    matcher = "200,202"
+  }
+}
+resource "aws_lb_listener" "alb_listener" {
+  load_balancer_arn = aws_lb.alb_web.arn
+  port = "80"
+  protocol = "HTTP"
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.alb_target_grp.arn
+  }
+}
+resource "aws_lb_target_group_attachment" "acq_target01" {
+  target_group_arn = aws_lb_target_group.alb_target_grp.arn
+  target_id = aws_instance.web_server_01.id
+  port = "80"
+}
+resource "aws_lb_target_group_attachment" "acq_target02" {
+  target_group_arn = aws_lb_target_group.alb_target_grp.arn
+  target_id = aws_instance.web_server_02
+  port = "80"
+}
